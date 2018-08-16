59639b620b08ae4a7a7b3835b0a69b952c1c2336
KatlegoMokoena <41574946+KatlegoMokoena@users.noreply.github.com>
2018-08-17 00:10:32 +0200

    saving my own butt

--- Simulator.py
+++ Simulator.py
@@ -5,6 +5,16 @@ Created on Tue Aug 14 20:13:37 2018
 @author: Em'kay
 """
 import numpy as np
+import matplotlib.pyplot as plt
+
+def plotter(data, runlength, title = "XvsY", Xaxis = "x", Yaxis= "y"):
+    pis = np.linspace(0,runlength,len(data))
+    plt.figure(1)
+    plt.plot(pis, data)
+    plt.title(title)
+    plt.xlabel(Xaxis)
+    plt.ylabel(Yaxis)
+    plt.show()
 
 def extractData(filename):
     file = open(filename, "r")
@@ -19,24 +29,27 @@ def extractData(filename):
     file.close() 
     return out
         
-def generateTraceFile(Capacity = 50, avgPacketSize = 1000):
-    servRate = ((Capacity*10**6)/avgPacketSize)*100
-    print (int(servRate))
+def generateTraceFile(Capacity = 50, avgPacketSize = 1000, workload = 0.8, 
+                      write = True):
+    traffic = 100
+    speed = (1-workload)*Capacity
+    packetsNum = ((speed*10**6)/avgPacketSize)
+    arrival = (traffic*10**6)/packetsNum
     file = open("testfile.txt","w") 
-    #test = np.random.exponential(1000*Capacity, 100000)
-    test = np.random.exponential(1000, int(servRate))
-    test2 = np.random.exponential(1000, int(servRate))
+    file.write("avgIntArrT_Fac,avgPackSize_Arr\n")
+    packet = np.random.exponential(avgPacketSize, int(packetsNum))
+    atime = np.random.exponential(arrival, int(packetsNum))
     
-    for i in range(len(test)):
-        file.write(str(int(test2[i])) + "," + str(int(test[i])) + "\n")
+    if write:
+        for i in range(len(atime)):
+            file.write(str(int(atime[i])) + "," + str(int(packet[i])) + "\n")
         
     temp = np.zeros(avgPacketSize)
     for i in range(avgPacketSize):
         temp[i] = (np.random.exponential(1000))
         
     file.close()
-    print (test.mean())
-    return test
+    return np.asarray([atime,packet]).transpose()
 
 def getAverages(Qdelay, increment = 10000, verbose = True):
     temp = []    
@@ -59,23 +72,37 @@ def getAverages(Qdelay, increment = 10000, verbose = True):
         print ("-----------------------------------")
     return np.asarray(av)
 
-def simulator(capacity = 1):        #  Takes link capacity in Mbps
-    filename = "testfile.txt"
-    #filename = "tracefile.txt"
-    data   = extractData(filename)
+def simulator(capacity, workload, filename = "tracefile.txt" , 
+              readFile = True, genQlen = True, verbose = True, write = True):        #  Takes link capacity in Mbps
+    if readFile:
+        data   = extractData(filename)
+    else:
+        data   = generateTraceFile(capacity, 1000, workload, write)
+  
     QDelay = np.zeros(len(data))    # Queueing delay 
     RTime  = np.zeros(len(data))    # Response time
     ATime  = np.zeros(len(data))    # Arrival time
-    PSize  = np.zeros(len(data))    # Packet size                   
+    PSize  = np.zeros(len(data))    # Packet size   
+    QLength = np.zeros(len(data))                
     lastMesTM = 0                   # last message recieved timestamp
     timer = 0
     transTime = 0
-    capacity = 1
-    
+
     for i in range(len(data)):
         message = data[i]
         ATime[i] = message[0]
         PSize[i] = message[1]
+        
+        if genQlen:
+            tempMT = lastMesTM
+            j = i
+            while (tempMT + data[j][0]) < timer:
+                tempMT = tempMT + data[j][0]
+                QLength[i] =  QLength[i] + 1
+                j = j+1
+                if (j == len(data)):
+                    break
+            
         if (lastMesTM + ATime[i]) > timer:
             QDelay[i] = lastMesTM + ATime[i] - timer
             timer = lastMesTM + ATime[i]
@@ -85,14 +112,40 @@ def simulator(capacity = 1):        #  Takes link capacity in Mbps
         RTime[i] = QDelay[i] + transTime
         timer = timer + transTime
         
-    getAverages(QDelay, 10000, False)
-    print ("Total running time in us: " + str(timer))
-    print ("Avarage Arrival time (λ) in us: " + str(ATime.mean()))
-    print (PSize.mean())
-    print ("Average packet size (μ): " + str((capacity*(10**6))/PSize.mean()))
-    print ("Average Response time in us: " + str(RTime.mean()))
-    print ("Average Queueing delay in us: " + str(QDelay.mean()))
+    #getAverages(QDelay, 10000, False) 
+    if(verbose):
+        print ("Total running time in s: " + str(timer/10**6))
+        print ("Avarage Arrival time (λ) in us: " + str(ATime.mean()))
+        print ("Average packet size (μ): " + str(PSize.mean()))
+        print ("Average Response time in ms: " + str(RTime.mean()/10**3))
+        print ("Average Queueing delay in ms: " + str(QDelay.mean()/10**3))
+    
+    return QDelay.mean(), QLength
 
-capacity = 5   # link capacity in Mbps
-test = generateTraceFile(capacity)
-simulator(capacity)
+def getWorkloadResults():
+    filename = "testfile.txt"
+    workload = np.linspace(0.1,1,10)
+    capacity = 100  # link capacity in Mbps
+    QdelayPWL = [] # average Queue delay per workload (0-100%)
+    k = -1
+    for workL in workload:
+        k = k+1
+        if k == 1:
+            continue
+        print ("iteration " + str(k))
+        avrQDelay = np.zeros(100)
+        for i in range(100):
+            QDelay, LQ= simulator(capacity, workL, filename, False, 
+                                  genQlen = False, verbose = False, 
+                                  write = False)
+            avrQDelay[i] = QDelay
+        plotter(avrQDelay, 100, "Queue delay per iteration with load = " + 
+                str(workL), "iteration", "Queue delay (us)" )
+        QdelayPWL.append(avrQDelay.mean())  
+    
+    plotter(QdelayPWL, 100, "Queue delay vs Workload", "Workload (%)", 
+            "Queue delay (us)" )
+    
+getWorkloadResults()
+filename = "testfile.txt"
+simulator(100, 0.4, filename, False, genQlen = False, verbose = True)
